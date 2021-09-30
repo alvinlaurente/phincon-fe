c62f8d832a2340e7814211adf379c4796c739792
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2021-09-30 16:59:28 +0700

    Boilerplate

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+function Header() {
+  return (
+    <header className="border-b p-3 flex justify-between items-centeryarn add @fortawesome/fontawesome-svg-core">
+      <span className="font-bold">
+        AppName
+      </span>
+      
+      <Navigation />
+    </header>
+  )
+}
+
+export default Header
